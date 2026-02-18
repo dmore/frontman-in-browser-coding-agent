471e5c0d26cea1377e5f7650c2ca979747e88a65
Itay Adler <itayadler@gmail.com>
2026-02-18 06:43:01 +0200

    Add videos to select-element and code-change in marketing

--- libs/frontman-astro/index.mjs
+++ libs/frontman-astro/index.mjs
@@ -1,3 +1,7 @@
 // Thin wrapper to provide `export default` for `astro add` CLI compatibility.
 // ReScript doesn't support default exports natively.
-export { frontmanIntegration as default, frontmanIntegration } from './src/FrontmanAstro.res.mjs';
+// NOTE: Using import + export default (not `export { x as default }`) because
+// tsup strips the re-export form during bundling.
+import { frontmanIntegration } from './src/FrontmanAstro.res.mjs';
+export default frontmanIntegration;
+export { frontmanIntegration };
